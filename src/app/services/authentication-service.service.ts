7b6604c1eddef9ca30877be2857d3a3f24c9eb17
marcio sansone <msansone@gmail.com>
2023-02-25 14:32:49 +0000

    fix

--- src/app/services/authentication-service.service.ts
+++ src/app/services/authentication-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.dev';
+import { environment } from 'src/environments/environment';
 import { Login } from '../model/login';
 import { Manager } from '../model/manager';
 
